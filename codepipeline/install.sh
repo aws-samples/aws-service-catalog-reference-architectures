293f14a50b013a47f46633a8a3b6d2ad7d01084a
Chris Chapman <chpmanc@amazon.com>
2018-12-27 15:06:52 -0500

    updated deployment scripts, reworked taskcat

---
+++ codepipeline/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This script will setup the Automated pipeline, IAM Roles, and a ServiceCatalog Portfolio using the 
+# reference architecture as example products.  This will create resources in your currently active CLI account
+# across three regions using CloudFormation StackSets.  You will be responsible for any costs resulting from the usage
+# of this script.
+
+ACC=$(aws sts get-caller-identity --query 'Account' | tr -d '"')
+export AWS_DEFAULT_REGION=us-east-1
+echo "Using Account:$ACC  Region:$AWS_DEFAULT_REGION"
+
+echo "Creating the StackSet IAM roles"
+aws cloudformation create-stack --region us-east-1 --stack-name IAM-StackSetAdministrator --template-url https://s3.amazonaws.com/cloudformation-stackset-sample-templates-us-east-1/AWSCloudFormationStackSetAdministrationRole.yml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --region us-east-1 --stack-name IAM-StackSetExecution --parameters "[{\"ParameterKey\":\"AdministratorAccountId\",\"ParameterValue\":\"$ACC\"}]" --template-url https://s3.amazonaws.com/cloudformation-stackset-sample-templates-us-east-1/AWSCloudFormationStackSetExecutionRole.yml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+echo "waiting for stacks to complete..."
+aws cloudformation wait stack-create-complete --stack-name IAM-StackSetAdministrator
+aws cloudformation wait stack-create-complete --stack-name IAM-StackSetExecution
+
+echo "creating the automation pipeline stack"
+aws cloudformation create-stack --region us-east-1 --stack-name SC-RA-IACPipeline --parameters '[{"ParameterKey":"ChildAccountAccess","ParameterValue":""}]' --template-url https://s3.amazonaws.com/aws-service-catalog-reference-architectures/codepipeline/sc-codepipeline-ra.json --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+
+echo "creating the ServiceCatalog IAM roles StackSet"
+aws cloudformation create-stack-set --stack-set-name SC-IAC-automated-IAMroles --template-url https://s3.amazonaws.com/aws-service-catalog-reference-architectures/iam/sc-demosetup-iam.json --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+SSROLEOPID=$(aws cloudformation create-stack-instances --stack-set-name SC-IAC-automated-IAMroles --regions '["us-east-1"]' --accounts $ACC --operation-preferences FailureToleranceCount=0,MaxConcurrentCount=1 | jq '.OperationId'  | tr -d '"')
+STATUS=""
+until [ "$STATUS" = "SUCCEEDED" ]; do 
+  STATUS=$(aws cloudformation describe-stack-set-operation --stack-set-name SC-IAC-automated-IAMroles --operation-id $SSROLEOPID | jq '.StackSetOperation.Status' | tr -d '"')
+  echo "waiting for IAMrole Stackset to complete. current status: $STATUS"
+  sleep 5
+done
+
+echo "creating the ServiceCatalog Portfolio StackSet"
+aws cloudformation create-stack-set --stack-set-name SC-IAC-automated-portfolio --parameters '[{"ParameterKey":"LinkedRole1","ParameterValue":""},{"ParameterKey":"LinkedRole2","ParameterValue":""},{"ParameterKey":"LaunchRoleName","ParameterValue":"SCEC2LaunchRole"},{"ParameterKey":"RepoRootURL","ParameterValue":"https://s3.amazonaws.com/aws-service-catalog-reference-architectures/"}]' --template-url https://s3.amazonaws.com/aws-service-catalog-reference-architectures/ec2/sc-portfolio-ec2VPC.json
+aws cloudformation create-stack-instances --stack-set-name SC-IAC-automated-portfolio --regions '["us-east-1","us-east-2","us-west-1"]' --accounts $ACC --operation-preferences FailureToleranceCount=0,MaxConcurrentCount=3
+
+echo "Complete.  See CloudFormation Stacks and StackSets Console in each region us-east1, us-east2, us-west-1 for more details."
