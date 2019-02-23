5d940e3cdf100b0f4ee7205b14242f341e47b752
Chris Chapman <chpmanc@amazon.com>
2019-02-22 21:33:58 -0500

    updated single account CF update script

--- codepipeline/run-cloudformationupdate.sh
+++ codepipeline/run-cloudformationupdate.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 echo "Using Account:$ACCID  Region:$AWS_DEFAULT_REGION"
-NOUPDATES=false
 ACCID=$(aws sts get-caller-identity --query 'Account' | tr -d '"')
-ESTR=$((aws cloudformation update-stack --stack-name SC-IAC-automated-IAMroles  --parameters "[{\"ParameterKey\":\"RepoRootURL\",\"ParameterValue\":\"https://s3.amazonaws.com/servicesatalog-deployedtemplates-$ACCID/\"}]" --template-url "https://s3.amazonaws.com/servicesatalog-deployedtemplates-$ACCID/iam/sc-demosetup-iam.json" --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND) 2>&1)
+ESTR=$((aws cloudformation update-stack --stack-name SC-IAC-automated-portfolio --parameters "[{\"ParameterKey\":\"LinkedRole2\",\"UsePreviousValue\":true},{\"ParameterKey\":\"LinkedRole1\",\"ParameterValue\":\"Admin\"},{\"ParameterKey\":\"LaunchRoleName\",\"UsePreviousValue\":true},{\"ParameterKey\":\"RepoRootURL\",\"ParameterValue\":\"https://s3.amazonaws.com/servicesatalog-deployedtemplates-$ACCID/\"}]" --template-url "https://s3.amazonaws.com/servicesatalog-deployedtemplates-$ACCID/ec2/sc-portfolio-ec2demo.json") 2>&1)
 ECODE=$?
-if [[ "$ECODE" -eq "255" && "$ESTR" =~ .(No updates are to be performed\.)$ ]] ; then 
-  NOUPDATES=true
-fi
-
-ESTR=$((aws cloudformation update-stack --stack-name SC-IAC-automated-portfolio --parameters "[{\"ParameterKey\":\"LinkedRole2\",\"UsePreviousValue\":true},{\"ParameterKey\":\"LinkedRole1\",\"UsePreviousValue\":true},{\"ParameterKey\":\"LaunchRoleName\",\"UsePreviousValue\":true},{\"ParameterKey\":\"RepoRootURL\",\"ParameterValue\":\"https://s3.amazonaws.com/servicesatalog-deployedtemplates-$ACCID/\"}]" --template-url "https://s3.amazonaws.com/servicesatalog-deployedtemplates-$ACCID/ec2/sc-portfolio-ec2demo.json") 2>&1)
-ECODE=$?
-if [[ "$ECODE" -eq "255" && "$ESTR" =~ .(No updates are to be performed\.)$ ]] ; then
-  NOUPDATES=true
-fi
-
-if [ "$NOUPDATES" = true ]
+if [[ "$ECODE" -eq "255" && "$ESTR" =~ .(No updates are to be performed\.)$ ]]
 then 
   echo "No updates, continue."
   exit 0
