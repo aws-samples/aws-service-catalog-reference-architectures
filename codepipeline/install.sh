e5ca5750431c46176f964f9fcffdae8bfa6f4a2d
Chris Chapman <chpmanc@amazon.com>
2019-05-17 10:16:59 -0400

    add region to s3 paths in codepipeline

--- codepipeline/install.sh
+++ codepipeline/install.sh
@@ -11,23 +11,23 @@ ACC=$(aws sts get-caller-identity --query 'Account' | tr -d '"')
 childAcc=""
 childAccComma=${childAcc// /,}
 allACC="$ACC $childAcc"
+export AWS_DEFAULT_REGION=us-east-1
 allregions="us-east-1 us-east-2 us-west-1"
 LinkedRole1=""
 S3RootURL="https://s3.amazonaws.com/aws-service-catalog-reference-architectures"
 
 date
-export AWS_DEFAULT_REGION=us-east-1
 echo "Using Account:$ACC  Region:$AWS_DEFAULT_REGION Child Accounts:$childAcc All Regions:$allregions"
 
 echo "Creating the StackSet IAM roles"
-aws cloudformation create-stack --region us-east-1 --stack-name IAM-StackSetAdministrator --template-url https://s3.amazonaws.com/cloudformation-stackset-sample-templates-us-east-1/AWSCloudFormationStackSetAdministrationRole.yml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
-aws cloudformation create-stack --region us-east-1 --stack-name IAM-StackSetExecution --parameters "[{\"ParameterKey\":\"AdministratorAccountId\",\"ParameterValue\":\"$ACC\"}]" --template-url https://s3.amazonaws.com/cloudformation-stackset-sample-templates-us-east-1/AWSCloudFormationStackSetExecutionRole.yml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --region $AWS_DEFAULT_REGION --stack-name IAM-StackSetAdministrator --template-url https://s3.amazonaws.com/cloudformation-stackset-sample-templates-us-east-1/AWSCloudFormationStackSetAdministrationRole.yml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --region $AWS_DEFAULT_REGION --stack-name IAM-StackSetExecution --parameters "[{\"ParameterKey\":\"AdministratorAccountId\",\"ParameterValue\":\"$ACC\"}]" --template-url https://s3.amazonaws.com/cloudformation-stackset-sample-templates-us-east-1/AWSCloudFormationStackSetExecutionRole.yml --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
 echo "waiting for stacks to complete..."
 aws cloudformation wait stack-create-complete --stack-name IAM-StackSetAdministrator
 aws cloudformation wait stack-create-complete --stack-name IAM-StackSetExecution
 
 echo "creating the automation pipeline stack"
-aws cloudformation create-stack --region us-east-1 --stack-name SC-RA-IACPipeline --parameters "[{\"ParameterKey\":\"ChildAccountAccess\",\"ParameterValue\":\"$childAccComma\"}]" --template-url "$S3RootURL/codepipeline/sc-codepipeline-ra.json" --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+aws cloudformation create-stack --region $AWS_DEFAULT_REGION --stack-name SC-RA-IACPipeline --parameters "[{\"ParameterKey\":\"ChildAccountAccess\",\"ParameterValue\":\"$childAccComma\"}]" --template-url "$S3RootURL/codepipeline/sc-codepipeline-ra.json" --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
 
 echo "creating the ServiceCatalog IAM roles StackSet"
 aws cloudformation create-stack-set --stack-set-name SC-IAC-automated-IAMroles --template-url "$S3RootURL/iam/sc-demosetup-iam.json" --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
--- codepipeline/run-pipelineupdate.sh
+++ codepipeline/run-pipelineupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Using Account:$ACCID  Region:$AWS_DEFAULT_REGION"
 ACCID=$(aws sts get-caller-identity --query 'Account' | tr -d '"')
-ESTR=$((aws cloudformation update-stack --stack-name SC-RA-IACPipeline --parameters '[{"ParameterKey":"ChildAccountAccess","UsePreviousValue":true}]' --template-url "https://s3.amazonaws.com/servicecatalog-deployedtemplates-$ACCID/codepipeline/sc-codepipeline-ra.json" --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND) 2>&1)
+ESTR=$((aws cloudformation update-stack --stack-name SC-RA-IACPipeline --parameters '[{"ParameterKey":"ChildAccountAccess","UsePreviousValue":true}]' --template-url "https://s3-$AWS_DEFAULT_REGION.amazonaws.com/servicecatalog-deployedtemplates-$ACCID/codepipeline/sc-codepipeline-ra.json" --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND) 2>&1)
 ECODE=$?
 if [[ "$ECODE" -eq "255" && "$ESTR" =~ .(No updates are to be performed\.)$ ]]
 then 
